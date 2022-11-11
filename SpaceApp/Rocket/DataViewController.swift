c7ace2559e87110ddf4fc9a0b18da3ce49ed2bcd
GregoryDushin <86513789+GregoryDushin@users.noreply.github.com>
2022-11-11 16:42:30 +0400

    synt fixes

--- SpaceApp/Rocket/DataViewController.swift
+++ SpaceApp/Rocket/DataViewController.swift
@@ -103,12 +103,20 @@ final class DataViewController: UIViewController {
             }
             switch section {
             case .image:
-                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
-                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.6)), subitems: [item])
+                let item = NSCollectionLayoutItem(
+                    layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+                )
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.6)), subitems: [item]
+                )
                 return NSCollectionLayoutSection(group: group)
             case .horizontal:
-                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
-                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .absolute(150), heightDimension: .absolute(150)), subitems: [item])
+                let item = NSCollectionLayoutItem(
+                    layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+                )
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: .init(widthDimension: .absolute(150), heightDimension: .absolute(150)), subitems: [item]
+                )
                 let section = NSCollectionLayoutSection(group: group)
                 section.orthogonalScrollingBehavior = .continuous
                 section.interGroupSpacing = 5
@@ -116,72 +124,128 @@ final class DataViewController: UIViewController {
                 return section
             case .vertical:
                 let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(60)))
-                let group = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(130)), subitems: [item])
+                let group = NSCollectionLayoutGroup.vertical(
+                    layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(130)), subitems: [item]
+                )
                 let section = NSCollectionLayoutSection(group: group)
                 // HEADER TROUBLE
                 let headerFooterSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(44))
-                let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerFooterSize,elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+                let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
+                    layoutSize: headerFooterSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top
+                )
                 section.boundarySupplementaryItems = [sectionHeader]
                 return section
             case .button:
-                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
-                let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(90)), subitems: [item])
+                let item = NSCollectionLayoutItem(
+                    layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+                )
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(90)), subitems: [item]
+                )
                 return NSCollectionLayoutSection(group: group)
-                
             }
         }
     }
-    
+
     // MARK: - Implementing data from URL into items
-    
+
     private func mapRocketToSections(rocket: RocketModelElement) -> [Section] {
-        [Section(sectionType: .image,
-                 title: nil,
-                 items: [.image(url: URL(string: rocket.flickrImages[0])!, rocketName: rocket.name)]),
-         Section(sectionType: .horizontal,
-                 title: nil,
-                 items:
-                    [.horizontalInfo(title: "Высота, ft",
-                                     value: String(rocket.height.meters ?? 0.0)),
-                     .horizontalInfo(title: "Диаметр, ft",
-                                     value: String(rocket.diameter.meters ?? 0.0)),
-                     .horizontalInfo(title: "Масса, lb",
-                                     value: String(rocket.mass.kg)),
-                     .horizontalInfo(title: "Нагрузка, lb",
-                                     value: String(rocket.payloadWeights[0].kg))]),
-         Section(sectionType: .vertical,
-                 title: nil,
-                 items:
-                    [.verticalInfo(title: "Первый запуск",
-                                   value: rocket.firstFlight,
-                                   id: UUID()),
-                     .verticalInfo(title: "Страна",
-                                   value: "США",
-                                   id: UUID()),
-                     .verticalInfo(title: "Стоимость запуска",
-                                   value: "$" + String((rocket.costPerLaunch)/1000000) + " млн",
-                                   id: UUID())]),
-         Section(sectionType: .vertical,
-                 title: "Первая ступень",
-                 items:
-                    [.verticalInfo(title: "Количество двигателей",
-                                   value: String(rocket.firstStage.engines),
-                                   id: UUID()),
-                     .verticalInfo(title: "Количество топлива",
-                                   value: (NSString(format: "%.0f", rocket.firstStage.fuelAmountTons) as String) + " тонн", id: UUID()),
-                     .verticalInfo(title: "Время сгорания",
-                                   value: String(rocket.firstStage.burnTimeSec ?? 0) + " сек", id: UUID())]),
-         Section(sectionType: .vertical, title: "Вторая ступень", items:
-                    [.verticalInfo(title: "Количество двигателей",
-                                   value: String(rocket.secondStage.engines),
-                                   id: UUID()),
-                     .verticalInfo(title: "Количество топлива",
-                                   value: (NSString(format: "%.0f", rocket.secondStage.fuelAmountTons) as String) + " тонн", id: UUID()),
-                     .verticalInfo(title: "Время сгорания",
-                                   value: String(rocket.secondStage.burnTimeSec ?? 0) + " сек",
-                                   id: UUID())
-                    ]),
-         Section(sectionType: .button, title: nil, items: [.button])
+        [
+            Section(
+                sectionType: .image,
+                title: nil,
+                items: [.image(url: URL(string: rocket.flickrImages[0])!, rocketName: rocket.name)]
+            ),
+            Section(
+                sectionType: .horizontal,
+                title: nil,
+                items:
+                    [
+                        .horizontalInfo(
+                            title: "Высота, ft",
+                            value: String(rocket.height.meters ?? 0.0)
+                        ),
+                        .horizontalInfo(
+                            title: "Диаметр, ft",
+                            value: String(rocket.diameter.meters ?? 0.0)
+                        ),
+                        .horizontalInfo(
+                            title: "Масса, lb",
+                            value: String(rocket.mass.kg)
+                        ),
+                        .horizontalInfo(
+                            title: "Нагрузка, lb",
+                            value: String(rocket.payloadWeights[0].kg)
+                        )
+                    ]
+            ),
+            Section(
+                sectionType: .vertical,
+                title: nil,
+                items:
+                    [
+                        .verticalInfo(
+                            title: "Первый запуск",
+                            value: rocket.firstFlight,
+                            id: UUID()
+                        ),
+                        .verticalInfo(
+                            title: "Страна",
+                            value: "США",
+                            id: UUID()
+                        ),
+                        .verticalInfo(
+                            title: "Стоимость запуска",
+                            value: "$" + String((rocket.costPerLaunch) / 1_000_000) + " млн",
+                            id: UUID()
+                        )
+                    ]
+            ),
+            Section(
+                sectionType: .vertical,
+                title: "Первая ступень",
+                items:
+                    [
+                        .verticalInfo(
+                            title: "Количество двигателей",
+                            value: String(rocket.firstStage.engines),
+                            id: UUID()
+                        ),
+                        .verticalInfo(
+                            title: "Количество топлива",
+                            value: (NSString(format: "%.0f", rocket.firstStage.fuelAmountTons) as String) + " тонн",
+                            id: UUID()
+                        ),
+                        .verticalInfo(
+                            title: "Время сгорания",
+                            value: String(rocket.firstStage.burnTimeSec ?? 0) + " сек",
+                            id: UUID()
+                        )
+                    ]
+            ),
+            Section(
+                sectionType: .vertical,
+                title: "Вторая ступень",
+                items:
+                    [
+                        .verticalInfo(
+                            title: "Количество двигателей",
+                            value: String(rocket.secondStage.engines),
+                            id: UUID()
+                        ),
+                        .verticalInfo(
+                            title: "Количество топлива",
+                            value: (NSString(format: "%.0f", rocket.secondStage.fuelAmountTons) as String) + " тонн",
+                            id: UUID()
+                        ),
+                        .verticalInfo(
+                            title: "Время сгорания",
+                            value: String(rocket.secondStage.burnTimeSec ?? 0) + " сек",
+                            id: UUID()
+                        )
+                    ]
+            ),
+            Section(sectionType: .button, title: nil, items: [.button])
         ]
     }
 
